4f766eec2f0bc5383f0ccfbc4a11925f04593c18
zoymelvin <101158364+zoymelvin@users.noreply.github.com>
2026-03-10 14:18:31 +0000

    Deploying to gh-pages from @ zoymelvin/VinFlow-app@f90a6243ed01540a6cb831bcae3c9767759a2dc0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2037667266" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1979970494" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
